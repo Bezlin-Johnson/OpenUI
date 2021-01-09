1564a5c1d51ddbb2ef4dcc0be5da3bfa99d1fefa
rithul123 <rithuldavis1120@gmail.com>
2021-01-09 23:25:22 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,1 +1,5 @@
-# hello
+import mysql.connector as myconc
+sq=myconc.connect(host='localhost',user='root',password="rithul",database="codeesa")
+if sq.is_connected():
+    print("connected")
+sqc=sq.cursor()
